013d15c6ca31c4f5d7f8ba6bd73df77eba26ed1a
Rafael B. Tauil <rafael@tauil.com.br>
2017-11-14 19:42:28 -0200

    Adds word guessing generator

--- bulls_and_cows_game.rb
+++ bulls_and_cows_game.rb
@@ -3,21 +3,46 @@
 
 require 'pry-byebug'
 
-@attempt = 0
+ONLY_LETTERS=/[a-z]/
+ALPHABET = ('a'..'z').to_a
+
+@attempts = []
 puts "Enter the desired secret word:"
 SECRET_WORD = gets.chomp
 
-puts "Picked number is: #{MAX}"
+def generate_guess
+  guess = []
+
+  # 4 letters word
+  while guess.size < 4
+    letter = ALPHABET[rand(ALPHABET.size)]
+    guess.push(letter) if !guess.include?(letter)
+  end
+
+  guess.join
+end
 
 def running()
-  puts "Attempt #{@attempt}"
+  puts "Attempt #{@attempts.size}"
+
+  @guess = generate_guess
 
   if @guess == SECRET_WORD
     puts "Secret word is: #{@guess}"
     return false
   end
 
-  @attempt += 1
+  puts "How many Bulls?"
+  @bulls = gets.chomp.to_i
+
+  puts "How many Cows?"
+  @cows = gets.chomp.to_i
+
+  if @bulls > 0 || @cows > 0
+    letters = @guess.scan(ONLY_LETTERS)
+    @attempts.push({bulls: @bulls, cows: @cows, letters: letters})
+  end
+
   true
 end
 
