9a66a250c91c964c1e077b8b326a01203da8bec5
Rafael B. Tauil <rafael@tauil.com.br>
2017-11-15 22:17:19 -0200

    Adds some more basic specs

--- spec/game_core_spec.rb
+++ spec/game_core_spec.rb
@@ -1,11 +1,44 @@
 require_relative '../lib/game_core'
 
 describe 'Bulls and Cows Game' do
-  let!(:game) { Game.new('word') }
+  let!(:secret_word) { 'word' }
+  let!(:game) { Game.new(secret_word) }
 
   describe '.initialize' do
     it 'sets dictionary' do
       expect(game.dictionary).to match(DICTIONARY)
+    end
+
+    it 'sets secret_word' do
+      expect(game.secret_word).to eq(secret_word)
+    end
+
+    it 'generates first guess' do
+      expect(game.guess).to_not be_nil
+    end
+  end
+
+  describe '#running?' do
+    context 'when secret_word is still not found' do
+      it 'returns true' do
+        expect(game.running?).to be_truthy
+      end
+    end
+
+    context 'when secret_word has been found' do
+      before { game.guess = secret_word }
+
+      it 'returns false' do
+        expect(game.running?).to be_falsy
+      end
+    end
+  end
+
+  describe '#status' do
+    before { game.guess = 'tesy' }
+
+    it 'displays current secret and current guess in a nicer way' do
+      expect(game.status).to eq(" ---------------------------------- \n   Current secret: word\n    Current guess: tesy\n ---------------------------------- ")
     end
   end
 end
